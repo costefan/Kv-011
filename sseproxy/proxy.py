86c7a52c80044b896f1fb1c2ba719fc7cf1f0c9a
Maxim Solomkin <solomkinmv@gmail.com>
2015-11-09 14:44:20 +0200

    Added Proxy Interface.

---
+++ sseproxy/proxy.py
@@ -0,0 +1,18 @@
+__author__ = 'maxim'
+
+
+class ProxyInterface(object):
+    """
+    Interface for Proxy.
+    """
+    def get_floor(self, floor):
+        raise NotImplementedError("Not implemented yet.")
+
+    def get_room(self, room):
+        raise NotImplementedError("Not implemented yet.")
+
+    def get_workspace(self, workspace):
+        raise NotImplementedError("Not implemented yet.")
+
+    def get_employee(self, employee):
+        raise NotImplementedError("Not implemented yet.")
